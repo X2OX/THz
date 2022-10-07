55f626dfc2df90d3c596127284aebfd847616aea
lixvda <1547446385@qq.com>
2022-10-07 15:06:03 +0800

    add: listen test

--- listen_test.go
+++ listen_test.go
@@ -1,9 +1,51 @@
 package THz
 
 import (
+	"fmt"
+	"net"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestAA(t *testing.T) {
+func TestListenAndServe(t *testing.T) {
+	thz := New()
+	thz.GET("/test", func(t *Context) {
+		t.JSON("hello world")
+	})
 
+	if err := thz.ListenAndServe(":8080"); err != nil {
+		fmt.Println("start err")
+	}
+}
+
+func TestRunAndStop(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	thz := New()
+	thz.GET("/run", func(t *Context) {
+		t.JSON("worked")
+	})
+
+	go func() {
+		time.Sleep(1 * time.Minute)
+		if err2 := thz.Stop(); err2 != nil {
+			fmt.Println("stop err")
+			fmt.Println(err2)
+		}
+		wg.Done()
+	}()
+
+	ln, err := net.Listen("tcp4", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err = thz.Run(ln); err != nil {
+		fmt.Println(err)
+	}
+
+	wg.Wait()
+	fmt.Println("finished")
 }
