25d525df53031a21ce890cd50d9904b03aab99bb
lixvda <1547446385@qq.com>
2022-10-08 13:48:51 +0800

    fix: data race
    fix: stop serve
    
    fix: data race
    
    fix: data race
    
    fix: error exit
    
    fix: test ci
    
    fix: test ci
    
    fix: for ci
    
    fix: test ci
    
    fix: test ci
    
    fix: data race

--- listen_test.go
+++ listen_test.go
@@ -3,7 +3,6 @@ package THz
 import (
 	"fmt"
 	"net"
-	"sync"
 	"testing"
 	"time"
 )
@@ -14,26 +13,30 @@ func TestListenAndServe(t *testing.T) {
 		t.JSON("hello world")
 	})
 
+	go func() {
+		time.Sleep(5 * time.Millisecond)
+		if err := thz.Stop(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
 	if err := thz.ListenAndServe(":8080"); err != nil {
 		fmt.Println("start err")
 	}
 }
 
 func TestRunAndStop(t *testing.T) {
-	var wg sync.WaitGroup
-	wg.Add(1)
 	thz := New()
 	thz.GET("/run", func(t *Context) {
 		t.JSON("worked")
 	})
 
 	go func() {
-		time.Sleep(1 * time.Minute)
+		time.Sleep(5 * time.Second)
 		if err2 := thz.Stop(); err2 != nil {
 			fmt.Println("stop err")
 			fmt.Println(err2)
 		}
-		wg.Done()
 	}()
 
 	ln, err := net.Listen("tcp4", ":8080")
@@ -46,6 +49,4 @@ func TestRunAndStop(t *testing.T) {
 		fmt.Println(err)
 	}
 
-	wg.Wait()
-	fmt.Println("finished")
 }
