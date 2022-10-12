1044ecc2500df60dcaa888dbdcaa7871eb0647f8
Pinkpig <59948049+Ignorant-lxf@users.noreply.github.com>
2022-10-12 14:24:27 +0800

    add listen test and fix data race (#5)

--- listen_test.go
+++ listen_test.go
@@ -1,9 +1,52 @@
 package THz
 
 import (
+	"fmt"
+	"net"
 	"testing"
+	"time"
 )
 
-func TestAA(t *testing.T) {
+func TestListenAndServe(t *testing.T) {
+	thz := New()
+	thz.GET("/test", func(t *Context) {
+		t.JSON("hello world")
+	})
+
+	go func() {
+		time.Sleep(5 * time.Millisecond)
+		if err := thz.Stop(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	if err := thz.ListenAndServe(":8080"); err != nil {
+		fmt.Println("start err")
+	}
+}
+
+func TestRunAndStop(t *testing.T) {
+	thz := New()
+	thz.GET("/run", func(t *Context) {
+		t.JSON("worked")
+	})
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		if err2 := thz.Stop(); err2 != nil {
+			fmt.Println("stop err")
+			fmt.Println(err2)
+		}
+	}()
+
+	ln, err := net.Listen("tcp4", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err = thz.Run(ln); err != nil {
+		fmt.Println(err)
+	}
 
 }
